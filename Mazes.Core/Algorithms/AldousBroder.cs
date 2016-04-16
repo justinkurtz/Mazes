7d3b6572f1fc1db7604e28fe1f93d34126199d76
Justin Kurtz <kurtz.justin@gmail.com>
2016-04-16 11:14:32 -0600

    Add Aldous Broder algorithm

---
+++ Mazes.Core/Algorithms/AldousBroder.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mazes.Core.Algorithms
+{
+    public class AldousBroder
+    {
+        public static Grid On(Grid grid)
+        {
+            var cell = grid.GetRandomCell();
+            var unvisited = grid.Count() - 1;
+
+            while (unvisited > 0)
+            {
+                var neighbor = cell.Neighbors.Random();
+
+                if (!neighbor.Links.Any())
+                {
+                    cell.Link(neighbor);
+                    unvisited--;
+                }
+
+                cell = neighbor;
+            }
+
+            return grid;
+        }
+    }
+}
---
+++ Mazes.Tests/Algorithms/AldousBroderTests.cs
@@ -0,0 +1,25 @@
+﻿using Mazes.Core;
+using Mazes.Core.Algorithms;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace Mazes.Tests.Algorithms
+{
+    [TestClass]
+    public class AldousBroderTests
+    {
+        private Grid grid;
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            grid = new Grid(5, 5);
+        }
+
+        [TestMethod]
+        public void TestAldousBroder()
+        {
+            AldousBroder.On(grid).ToBitmap().Save("aldousbroder.png");
+        }
+    }
+}
