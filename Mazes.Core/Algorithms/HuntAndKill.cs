60c88a37ef5331a32384f02e53de76df0e966413
Justin Kurtz <kurtz.justin@gmail.com>
2016-04-17 16:15:55 -0600

    Add Hunt and Kill algorithm

---
+++ Mazes.Core/Algorithms/HuntAndKill.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+
+namespace Mazes.Core.Algorithms
+{
+    public class HuntAndKill
+    {
+        public static Grid On(Grid grid)
+        {
+            var current = grid.GetRandomCell();
+
+            while (current != null)
+            {
+                var unvisitedNeighbors = current.Neighbors.Where(n => !n.Links.Any());
+
+                if (unvisitedNeighbors.Any())
+                {
+                    var neighbor = unvisitedNeighbors.Random();
+                    current.Link(neighbor);
+                    current = neighbor;
+                }
+                else
+                {
+                    current = null;
+
+                    foreach (Cell cell in grid)
+                    {
+                        var visitedNeighbors = cell.Neighbors.Where(n => n.Links.Any());
+                        if (!cell.Links.Any() && visitedNeighbors.Any())
+                        {
+                            current = cell;
+                            var neighbor = visitedNeighbors.Random();
+                            current.Link(neighbor);
+                            break;
+                        }
+                    }
+                }
+            }
+
+            return grid;
+        }
+    }
+}
---
+++ Mazes.Tests/Algorithms/HuntAndKillTests.cs
@@ -0,0 +1,26 @@
+﻿using Mazes.Core;
+using Mazes.Core.Algorithms;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Mazes.Tests.Algorithms
+{
+    [TestClass]
+    public class HuntAndKillTests
+    {
+        [TestMethod]
+        public void TestHuntAndKill()
+        {
+            var grid = new Grid(5, 5);
+            HuntAndKill.On(grid).ToBitmap().Save("huntandkill.png");
+        }
+
+        [TestMethod]
+        public void TestHuntAndKillColored()
+        {
+            var coloredGrid = new ColoredGrid(25, 25);
+            HuntAndKill.On(coloredGrid);
+            coloredGrid.Distances = coloredGrid.GetCenterCell().Distances;
+            coloredGrid.ToBitmap().Save("huntandkill-colored.png");
+        }
+    }
+}
