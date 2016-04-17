21ab2d62b119f7cf84bffaca4ffdeb4e8e5a97b9
Justin Kurtz <kurtz.justin@gmail.com>
2016-04-17 16:07:28 -0600

    Add Sidewinder algorithm

---
+++ Mazes.Core/Algorithms/Sidewinder.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mazes.Core.Algorithms
+{
+    public class Sidewinder
+    {
+        private static Random rand = new Random();
+
+        public static Grid On(Grid grid)
+        {
+            foreach (var row in grid.EachRow())
+            {
+                var run = new List<Cell>();
+
+                foreach(var cell in row)
+                {
+                    run.Add(cell);
+                    var closeOut = cell.East == null || (cell.North != null && rand.Next(2) == 0);
+
+                    if (closeOut)
+                    {
+                        var member = run.Random();
+                        if (member.North != null)
+                        {
+                            member.Link(member.North);
+                        }
+                        run.Clear();
+                    }
+                    else
+                    {
+                        cell.Link(cell.East);
+                    }
+                }
+            }
+
+            return grid;
+        }
+    }
+}
---
+++ Mazes.Tests/Algorithms/SidewinderTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mazes.Core.Algorithms;
+using Mazes.Core;
+
+namespace Mazes.Tests.Algorithms
+{
+    [TestClass]
+    public class SidewinderTests
+    {
+        [TestMethod]
+        public void TestSidewinder()
+        {
+            var grid = new Grid(5, 5);
+            Sidewinder.On(grid).ToBitmap().Save("sidewinder.png");
+        }
+
+        [TestMethod]
+        public void TestBinaryTreeColoredGrid()
+        {
+            var coloredGrid = new ColoredGrid(25, 25);
+            Sidewinder.On(coloredGrid);
+            coloredGrid.Distances = coloredGrid.GetCenterCell().Distances;
+            coloredGrid.ToBitmap().Save("sidewinder-colored.png");
+        }
+    }
+}
