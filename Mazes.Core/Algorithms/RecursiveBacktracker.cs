b2c94973e5209d061f1dc4bff9f2302d949d0d6c
Justin Kurtz <kurtz.justin@gmail.com>
2016-04-17 16:26:10 -0600

    Add Recursive Backtracker algorithm

---
+++ Mazes.Core/Algorithms/RecursiveBacktracker.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Mazes.Core.Algorithms
+{
+    public class RecursiveBacktracker
+    {
+        public static Grid On(Grid grid, Cell start = null)
+        {
+            if (start == null)
+            {
+                start = grid.GetRandomCell();
+            }
+
+            var stack = new Stack<Cell>();
+            stack.Push(start);
+
+            while(stack.Any())
+            {
+                var current = stack.Peek();
+                var neighbors = current.Neighbors.Where(n => !n.Links.Any());
+
+                if (neighbors.Any())
+                {
+                    var neighbor = neighbors.Random();
+                    current.Link(neighbor);
+                    stack.Push(neighbor);
+                }
+                else
+                {
+                    stack.Pop();
+                }
+            }
+
+            return grid;
+        }
+    }
+}
---
+++ Mazes.Tests/Algorithms/RecursiveBacktrackerTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mazes.Core.Algorithms;
+using Mazes.Core;
+
+namespace Mazes.Tests.Algorithms
+{
+    [TestClass]
+    public class RecursiveBacktrackerTests
+    {
+        [TestMethod]
+        public void TestRecursiveBacktracker()
+        {
+            var grid = new Grid(5, 5);
+            RecursiveBacktracker.On(grid).ToBitmap().Save("recursivebacktracker.png");
+        }
+
+        [TestMethod]
+        public void TestRecursiveBacktrackerColored()
+        {
+            var coloredGrid = new ColoredGrid(25, 25);
+            RecursiveBacktracker.On(coloredGrid);
+            coloredGrid.Distances = coloredGrid.GetCenterCell().Distances;
+            coloredGrid.ToBitmap().Save("recursivebacktracker-colored.png");
+        }
+    }
+}
