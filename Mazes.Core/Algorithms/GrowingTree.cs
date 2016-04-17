78a27848b039337332e2490e4b006a51846b9bbd
Justin Kurtz <kurtz.justin@gmail.com>
2016-04-17 16:49:14 -0600

    Implement Growing Tree algorithm

---
+++ Mazes.Core/Algorithms/GrowingTree.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Mazes.Core.Algorithms
+{
+    public class GrowingTree
+    {
+        public static Grid On(Grid grid, Cell start = null)
+        {
+            if (start == null)
+            {
+                start = grid.GetRandomCell();
+            }
+
+            var active = new List<Cell>();
+            active.Add(start);
+
+            while(active.Any())
+            {
+                var cell = active.Last();
+                var neighbors = cell.Neighbors.Where(n => !n.Links.Any());
+
+                if (neighbors.Any())
+                {
+                    var neighbor = neighbors.Random();
+                    cell.Link(neighbor);
+                    active.Add(neighbor);
+                }
+                else
+                {
+                    active.Remove(cell);
+                }
+            }
+
+            return grid;
+        }
+    }
+}
---
+++ Mazes.Tests/Algorithms/GrowingTreeTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mazes.Core;
+using Mazes.Core.Algorithms;
+
+namespace Mazes.Tests.Algorithms
+{
+    [TestClass]
+    public class GrowingTreeTests
+    {
+        [TestMethod]
+        public void TestGrowingTree()
+        {
+            var grid = new Grid(5, 5);
+            GrowingTree.On(grid).ToBitmap().Save("growingtree.png");
+        }
+
+        [TestMethod]
+        public void TestGrowingTreeColored()
+        {
+            var coloredGrid = new ColoredGrid(25, 25);
+            GrowingTree.On(coloredGrid);
+            coloredGrid.Distances = coloredGrid.GetCenterCell().Distances;
+            coloredGrid.ToBitmap().Save("growingtree-colored.png");
+        }
+    }
+}
