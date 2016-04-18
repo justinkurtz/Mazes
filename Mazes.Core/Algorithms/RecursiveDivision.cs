49d07036a586b944e150e51321b55a1d3912005d
Justin Kurtz <kurtz.justin@gmail.com>
2016-04-17 19:03:22 -0600

    Add Recursive Division algorithm

--- Mazes.Core/Algorithms/RecursiveDivision.cs
+++ Mazes.Core/Algorithms/RecursiveDivision.cs
@@ -4,9 +4,70 @@ namespace Mazes.Core.Algorithms
 {
     public class RecursiveDivision
     {
+        private static Random rand = new Random();
+        private static Grid dividingGrid { get; set; }
+
         public static Grid On(Grid grid)
         {
-            throw new NotImplementedException();
+            dividingGrid = grid;
+
+            foreach (Cell cell in grid)
+            {
+                cell.Neighbors.ForEach(n => cell.Link(n, false));
+            }
+
+            Divide(0, 0, grid.Rows, grid.Columns);
+            return grid;
+        }
+
+        private static void Divide(int row, int col, int height, int width)
+        {
+            if (height <= 1 || width <= 1)
+                return;
+
+            if (height > width)
+            {
+                DivideHorizontally(row, col, height, width);
+            }
+            else
+            {
+                DivideVertically(row, col, height, width);
+            }
+        }
+
+        private static void DivideHorizontally(int row, int column, int height, int width)
+        {
+            var divideSouthOf = rand.Next(height - 1);
+            var passageAt = rand.Next(width);
+            for (int i = 0; i < width; i++)
+            {
+                if (passageAt == i)
+                    continue;
+
+                var cell = dividingGrid[row + divideSouthOf, column + i];
+                cell.Unlink(cell.South);
+            }
+
+            Divide(row, column, divideSouthOf + 1, width);
+            Divide(row + divideSouthOf + 1, column, height - divideSouthOf - 1, width);
+        }
+
+        private static void DivideVertically(int row, int column, int height, int width)
+        {
+            var divideEastOf = rand.Next(width - 1);
+            var passageAt = rand.Next(height);
+
+            for (int i = 0; i < height; i++)
+            {
+                if (passageAt == i)
+                    continue;
+
+                var cell = dividingGrid[row + i, column + divideEastOf];
+                cell.Unlink(cell.East);
+            }
+
+            Divide(row, column, height, divideEastOf + 1);
+            Divide(row, column + divideEastOf + 1, height, width - divideEastOf - 1);
         }
     }
 }
---
+++ Mazes.Tests/Algorithms/RecursiveDivisionTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mazes.Core.Algorithms;
+using Mazes.Core;
+
+namespace Mazes.Tests.Algorithms
+{
+    [TestClass]
+    public class RecursiveDivisionTests
+    {
+        [TestMethod]
+        public void TestRecursiveDivision()
+        {
+            var grid = new Grid(5, 5);
+            RecursiveDivision.On(grid).ToBitmap().Save("recursivedivision.png");
+        }
+
+        [TestMethod]
+        public void TestRecursiveDivisionColoredGrid()
+        {
+            var coloredGrid = new ColoredGrid(25, 25);
+            RecursiveDivision.On(coloredGrid);
+            coloredGrid.Distances = coloredGrid.GetCenterCell().Distances;
+            coloredGrid.ToBitmap().Save("recursivedivision-colored.png");
+        }
+    }
+}
